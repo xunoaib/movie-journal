b056825696e6ad59ac7759c8a49914043528786d
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-08-18 02:48:27 -0400

    Fix closed file

--- parsers/imdb.py
+++ parsers/imdb.py
@@ -6,5 +6,5 @@ from models import ImdbEntry
 def parse_imdb_movies(csv_fname):
     with open(csv_fname) as f:
         reader = csv.reader(f)
-    movies = [ImdbEntry(*row) for row in reader]
+        movies = [ImdbEntry(*row) for row in reader]
     return sorted(movies, key=lambda m: m.year)
