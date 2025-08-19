ca9f1d58d65d8f23d5395f20748a0552f0d2b3a5
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-08-19 15:58:36 -0400

    Move TSV/CSV logic into a function

--- parse_tsv_gzs_to_csv.py
+++ parse_tsv_gzs_to_csv.py
@@ -2,56 +2,63 @@
 Parses IMDb datasets into a CSV containing: tid,title,year,directors
 '''
 
-import polars as pl
 
-CSV_OPTS = dict(
-    separator="\t",
-    null_values=["\\N"],
-    infer_schema_length=0,
-    quote_char=None,
-    has_header=True,
-)
+def main():
+    import polars as pl
 
-basics = (
-    pl.scan_csv("title.basics.tsv.gz", **CSV_OPTS).filter(
-        (pl.col("titleType") == "movie")
-        & (pl.col("isAdult") == "0")
-        & (pl.col("startYear").is_not_null())
-    ).with_columns(
-        pl.col("startYear").cast(pl.Int32).alias("year"),
-        pl.col("primaryTitle").alias("title"),
-    ).select(["tconst", "title", "year"])
-)
-
-crew = (
-    pl.scan_csv("title.crew.tsv.gz",
-                **CSV_OPTS).select(["tconst", "directors"])
-)
-
-names = (
-    pl.scan_csv("name.basics.tsv.gz",
-                **CSV_OPTS).select(["nconst", "primaryName"])
-)
-
-# explode directors safely (skip null/empty)
-exploded = (
-    crew.
-    filter(pl.col("directors").is_not_null()
-           & (pl.col("directors") != "")).with_columns(
-               pl.col("directors").str.split(",")
-           ).explode("directors").rename({
-               "directors": "nconst"
-           }).join(names, on="nconst",
-                   how="inner").rename({"primaryName": "director"})
-)
-
-movie_directors_exploded = basics.join(exploded, on="tconst", how="inner")
-
-movie_directors = (
-    movie_directors_exploded.group_by(["tconst", "title", "year"]).agg(
-        pl.col("director").sort().str.join(", ").alias("directors")
+    CSV_OPTS = dict(
+        separator="\t",
+        null_values=["\\N"],
+        infer_schema_length=0,
+        quote_char=None,
+        has_header=True,
     )
-)
 
-# movie_directors_exploded.sink_csv("movie_directors_exploded.csv")
-movie_directors.sink_csv("movie_directors.csv")
+    basics = (
+        pl.scan_csv("title.basics.tsv.gz", **CSV_OPTS).filter(
+            (pl.col("titleType") == "movie")
+            & (pl.col("isAdult") == "0")
+            & (pl.col("startYear").is_not_null())
+        ).with_columns(
+            pl.col("startYear").cast(pl.Int32).alias("year"),
+            pl.col("primaryTitle").alias("title"),
+        ).select(["tconst", "title", "year"])
+    )
+
+    crew = (
+        pl.scan_csv("title.crew.tsv.gz",
+                    **CSV_OPTS).select(["tconst", "directors"])
+    )
+
+    names = (
+        pl.scan_csv("name.basics.tsv.gz",
+                    **CSV_OPTS).select(["nconst", "primaryName"])
+    )
+
+    # explode directors safely (skip null/empty)
+    exploded = (
+        crew.filter(
+            pl.col("directors").is_not_null()
+            & (pl.col("directors") != "")
+        ).with_columns(
+            pl.col("directors").str.split(",")
+        ).explode("directors").rename({
+            "directors": "nconst"
+        }).join(names, on="nconst",
+                how="inner").rename({"primaryName": "director"})
+    )
+
+    movie_directors_exploded = basics.join(exploded, on="tconst", how="inner")
+
+    movie_directors = (
+        movie_directors_exploded.group_by(["tconst", "title", "year"]).agg(
+            pl.col("director").sort().str.join(", ").alias("directors")
+        )
+    )
+
+    # movie_directors_exploded.sink_csv("movie_directors_exploded.csv")
+    movie_directors.sink_csv("movie_directors.csv")
+
+
+if __name__ == '__main__':
+    main()
