77b2afecc203189c6785cd0d5ea7b6be6c8ec3ed
xunoaib <13055788+xunoaib@users.noreply.github.com>
2025-08-21 21:18:36 -0400

    Update IMDb parser to use DictReader for parsing CSV

--- parsers/imdb.py
+++ parsers/imdb.py
@@ -5,6 +5,13 @@ from models import ImdbEntry
 
 def parse_imdb_movies(csv_fname):
     with open(csv_fname) as f:
-        reader = csv.reader(f)
-        movies = [ImdbEntry(*row) for row in reader]
+        reader = csv.DictReader(f)
+        movies = [
+            ImdbEntry(
+                tid=row['tconst'],
+                title=row['title'],
+                year=row['year'],
+                director=row['directors'],
+            ) for row in reader
+        ]
     return sorted(movies, key=lambda m: m.year)
